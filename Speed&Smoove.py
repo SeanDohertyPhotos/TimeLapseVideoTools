bef7b41128b0a6a1f1dabe87c1de37a8d44453da
smaster7772 <Smaster7772@gmail.com>
2023-09-30 16:32:15 -0500

    Added Multiprocessing

--- Speed&Smoove.py
+++ Speed&Smoove.py
@@ -1,66 +1,68 @@
-import cv2
+from multiprocessing import Pool, cpu_count
 import numpy as np
-import threading
+import cv2
 from tkinter import filedialog
 from tqdm import tqdm
+import time
 import os
 
-
-def process_video(input_filename, speed_up_factor):
-    # Open the video file.
+def process_video(args):
+    input_filename, speed_up_factor, total_cores = args
     cap = cv2.VideoCapture(input_filename)
 
-    # Generate the output filename.
     base, ext = os.path.splitext(input_filename)
     output_filename = f"{base}_{speed_up_factor}x_timelapse{ext}"
 
-    # Get the total number of frames in the video.
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    # Create a VideoWriter object to write the output video.
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(output_filename, fourcc, 30, (int(cap.get(3)), int(cap.get(4))))
 
-    frames = []
-    for _ in tqdm(range(total_frames), desc=f"Processing {input_filename}"):
+    print(f"Processing {input_filename} on {total_cores} cores with batch size of {speed_up_factor}")
+
+    running_sum = None
+    count = 0
+
+    start_time = time.time()
+    for _ in tqdm(range(total_frames), desc=f"Processing {input_filename}", unit="frame", unit_scale=True):
         ret, frame = cap.read()
         if not ret:
             break
-        frames.append(frame)
-        if len(frames) == speed_up_factor:
-            avg_frame = np.mean(frames, axis=0).astype(np.uint8)
+        if running_sum is None:
+            running_sum = np.array(frame, dtype=np.float)
+        else:
+            running_sum += frame
+        count += 1
+        if count == speed_up_factor:
+            avg_frame = (running_sum / count).astype(np.uint8)
             out.write(avg_frame)
-            frames = []
+            running_sum = None
+            count = 0
 
-    # If there are any remaining frames, average and write them.
-    if frames:
-        avg_frame = np.mean(frames, axis=0).astype(np.uint8)
+    end_time = time.time()
+    processing_time = end_time - start_time
+    fps = total_frames / processing_time
+
+    if running_sum is not None:
+        avg_frame = (running_sum / count).astype(np.uint8)
         out.write(avg_frame)
 
-    # Release the VideoCapture and VideoWriter objects.
     cap.release()
     out.release()
     print(f"Processing completed for {input_filename}. Output saved as {output_filename}")
-
+    print(f"Processed at {fps:.2f} frames/sec")
 
 def main():
-    # Ask the user for the speed-up factor.
     speed_up_factor = int(input("Enter the speed-up factor: "))
-
-    # Show an "Open" dialog box and return the path to the selected file(s).
     filenames = filedialog.askopenfilenames(filetypes=[("MP4 files", "*.mp4")])
+    total_cores = cpu_count()
 
-    threads = []
-    for filename in filenames:
-        t = threading.Thread(target=process_video, args=(filename, speed_up_factor))
-        t.start()
-        threads.append(t)
+    print(f"Using {total_cores} cores for processing.")
 
-    for t in threads:
-        t.join()
+    with Pool(total_cores) as pool:
+        pool.map(process_video, [(filename, speed_up_factor, total_cores) for filename in filenames])
 
     print("All processing completed.")
-
 
 if __name__ == "__main__":
     main()
